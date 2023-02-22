179cecdd59774dd675b131f74ad33e405cefef7a
nabesteron <49988463+nabesteron@users.noreply.github.com>
2023-02-22 16:24:52 -0500

    build categories container,map over categories Arr

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,39 @@
 
 function App() {
+  const categories = [{
+    id: 1,
+    title: "Hats",
+  }
+    , {
+    id: 2,
+    title: 'Jackets',
+  }, {
+    id: 3,
+    title: 'Sneakers',
+  }, {
+    id: 4,
+    title: 'Mens',
+  }, {
+    id: 5,
+    title: 'Womens',
+  }]
 
 
   return (
-    <div className="App">
-hello
+    <div className="categories-container">
+      {categories.map(({ title }) => {
+        return (
+
+          <div className="category-container">
+            {/* <img/> */}
+            <div className="category-body-container">
+              <h2>{title}</h2>
+              <p>Shop Now</p>
+            </div>
+          </div>
+        )
+
+      })}
     </div>
   )
 }
